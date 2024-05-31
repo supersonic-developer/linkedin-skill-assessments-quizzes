b1ab0806600941e855d94009e6f07777647109bb
supersonic-developer <novozanszkzsombor@gmail.com>
2024-05-31 16:07:15 +0200

    Non English files were removed. File initializer python script was added, image renaming doesn't work correctly.

---
+++ non-english_remover.py
@@ -0,0 +1,68 @@
+import os
+import re
+
+class FileModifier:
+    def __init__(self):
+        self.root_dir = os.path.dirname(os.path.abspath(__file__))
+        self.subdirs = self.ListSubDirs()
+
+    def delete_non_english_files(self):
+        # Iterate through all the subdirectories in the root directory
+        for subdir in self.subdirs:
+            # Iterate through all files in the subdirectory
+            for file_name in os.listdir(subdir):
+                file_path = os.path.join(subdir, file_name)
+                if os.path.isfile(file_path):
+                    # Check if the file is a non-English quiz file
+                    if file_name.endswith('.md') and '-quiz-' in file_name:
+                        print(f"Deleting non-English file: {file_path}")
+                        os.remove(file_path)
+
+    def ListSubDirs(self):
+        subdirs = []
+        # Iterate through all the subdirectories in the root directory
+        for subdir in os.listdir(self.root_dir):
+            subdir_path = os.path.join(self.root_dir, subdir)
+            if os.path.isdir(subdir_path):
+                subdirs.append(subdir_path)
+        return subdirs
+
+    def rename_images_and_update_references(self):
+        for subdir in self.subdirs:
+            images_path = os.path.join(subdir, 'images')
+            if os.path.isdir(images_path):
+                imageCnt = 0
+                # Rename image files
+                for image_file in os.listdir(images_path):
+                    image_file_path = os.path.join(images_path, image_file)
+                    if os.path.isfile(image_file_path):
+                        imageCnt += 1
+                        new_image_name = f"picture{imageCnt}{os.path.splitext(image_file_path)[1]}"
+                        new_image_path = os.path.join(images_path, new_image_name)
+                        os.rename(image_file_path, new_image_path)
+                        print(f"Renamed {image_file_path} to {new_image_path}")
+
+                        # Update references in markdown files
+                        for md_file in os.listdir(subdir):
+                            if md_file.endswith('.md'):
+                                md_file_path = os.path.join(subdir, md_file)
+                                self.update_image_references(md_file_path, image_file, new_image_name)
+
+    def update_image_references(self, md_file_path, old_image_name, new_image_name):
+        with open(md_file_path, 'r', encoding='utf-8') as file:
+            content = file.read()
+
+        updated_content = re.sub(r'(!\[.*?\]\(images/' + re.escape(old_image_name) + r'\))',
+                                r'![\1](images/' + re.escape(new_image_name) + r')',
+                                content)
+
+        with open(md_file_path, 'w', encoding='utf-8') as file:
+            file.write(updated_content)
+        print(f"Updated references in {md_file_path}: {old_image_name} -> {new_image_name}")
+
+    # Specify the root directory where the script is located
+    root_directory = os.path.dirname(os.path.abspath(__file__))
+
+
+fileModifier = FileModifier()
+fileModifier.delete_non_english_files()
